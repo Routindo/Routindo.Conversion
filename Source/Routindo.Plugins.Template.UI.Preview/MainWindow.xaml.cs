e8dd0cd196c58b21457070ed66c6ee5df0f0d82e
Younes CHEIKH <1327480+YounesCheikh@users.noreply.github.com>
2021-05-08 05:10:25 +0200

    Initial commit

---
+++ Source/Routindo.Plugins.Template.UI.Preview/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace Routindo.Plugins.Template.UI.Preview
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
